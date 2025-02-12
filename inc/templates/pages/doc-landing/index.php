0fcc9156de30711428f1afab9291fa593f0be098
J7 <j7.dev.gg@gmail.com>
2025-02-12 20:38:39 +0800

    feat: landing 手機板

--- inc/templates/pages/doc-landing/index.php
+++ inc/templates/pages/doc-landing/index.php
@@ -23,11 +23,11 @@ $children_posts = get_posts(
 
 Plugin::get('hero');
 
-echo /* html */'<div class="container mx-auto mt-8">';
+echo /* html */'<div class="container mx-auto mt-8 px-4">';
 
 // 所有分類區塊
 echo /* html */'<h6 class="text-2xl text-content mb-6">瀏覽所有分類</h6>';
-echo /* html */'<div class="grid grid-cols-3 gap-14">';
+echo /* html */'<div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 md:gap-8 xl:gap-14">';
 
 foreach ($children_posts as $child_post) {
 	Plugin::get(
