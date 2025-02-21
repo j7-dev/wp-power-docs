c3ba35d5605cffd4908f3898d231402e11077e43
J7 <j7.dev.gg@gmail.com>
2025-02-21 14:03:01 +0800

    fix: 如果不是知識庫，就不覆寫版型

--- inc/classes/Domains/Doc/Templates.php
+++ inc/classes/Domains/Doc/Templates.php
@@ -23,7 +23,7 @@ final class Templates {
 	}
 
 	/**
-	 * 覆寫課程商品頁面
+	 * 覆寫知識庫頁面
 	 * [危險] 如果全域變數汙染，會導致無法預期行為
 	 *
 	 * @param string $template 原本的模板路徑
@@ -32,16 +32,22 @@ final class Templates {
 	 */
 	public function template_override( $template ) {
 
+		global $post;
+		$post_type = $post?->post_type;
+		if ($post_type !== CPT::POST_TYPE) {
+			return $template;
+		}
+
 		// 檢查主題複寫存不存在，不存在就用預設的
-		$post_type           = CPT::POST_TYPE;
+		$doc_post_type       = CPT::POST_TYPE;
 		$dir                 = \get_stylesheet_directory();
-		$theme_template_path = \wp_normalize_path("{$dir}/single-{$post_type}.php");
+		$theme_template_path = \wp_normalize_path("{$dir}/single-{$doc_post_type}.php");
 
 		if (file_exists($theme_template_path)) {
 			return $theme_template_path;
 		}
 
-		return \wp_normalize_path(Plugin::$dir . "/inc/templates/single-{$post_type}.php");
+		return \wp_normalize_path(Plugin::$dir . "/inc/templates/single-{$doc_post_type}.php");
 	}
 
 	/**
