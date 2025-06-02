6303aaea5343521cfc8c2128fd29ea8bfa0f07eb
J7 <j7.dev.gg@gmail.com>
2025-06-02 12:39:15 +0800

    chore

--- inc/classes/Utils/Base.php
+++ inc/classes/Utils/Base.php
@@ -1,16 +1,10 @@
 <?php
-/**
- * Base
- */
 
 declare (strict_types = 1);
 
 namespace J7\PowerDocs\Utils;
 
-/**
- * Class Utils
- */
+/** Class Utils */
 abstract class Base {
 	const APP1_SELECTOR = '#power_docs';
-	const DEFAULT_IMAGE = 'http://1.gravatar.com/avatar/1c39955b5fe5ae1bf51a77642f052848?s=96&d=mm&r=g';
 }
